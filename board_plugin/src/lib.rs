ac48bb5f358c3fd8ec60ac2363a084561990e8a6
youki21 <ms.14f.gelgoog.m@gmail.com>
2024-09-15 19:27:51 +0900

    chapter5

--- board_plugin/src/lib.rs
+++ board_plugin/src/lib.rs
@@ -2,9 +2,13 @@ pub mod components;
 pub mod resources;
 
 use bevy::log;
+use bevy::log::tracing_subscriber::fmt::format;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
+use components::Bomb;
+use components::BombNeighbor;
 use components::Coordinates;
+use resources::tile::Tile;
 use resources::BoardOption;
 use resources::TileSize;
 use resources::tile_map::TileMap;
@@ -24,6 +28,7 @@ impl BoardPlugin {
         mut commands: Commands,
         board_options: Option<Res<BoardOptions>>,
         window: Query<&Window, With<PrimaryWindow>>,
+        asset_server: Res<AssetServer>,
     ){
         let options = match board_options {
             Some(o) => o.clone(),
@@ -54,6 +59,8 @@ impl BoardPlugin {
             }
             BoardOption::Custom(p) => p,
         };
+        let font = asset_server.load("fonts/pixeled.ttf");
+        let bomb_image = asset_server.load("sprites/bomb.png");
         commands
             .spawn_empty()
             .insert(Name::new("Board"))
@@ -71,31 +78,107 @@ impl BoardPlugin {
                         ..Default::default()
                     })
                     .insert(Name::new("Background"));
-                for (y, line) in tile_map.iter().enumerate() {
-                    for (x, tile) in line.iter().enumerate() {
-                        parent
-                            .spawn(SpriteBundle {
+                Self::spawn_tile(
+                    parent,
+                    &tile_map,
+                    tile_size,
+                    options.tile_padding,
+                    Color::srgba(0.3, 0.3, 0.3, 1.0),
+                    bomb_image,
+                    font)
+            });
+    }
+
+    fn bomb_count_text_bundle(count: u8, font: Handle<Font>, size: f32) -> Text2dBundle{
+        // We ritrieve the text and the correct color
+        let (text, color) = (
+            count.to_string(),
+            match count {
+                1 => Color::WHITE,
+                2 => Color::srgba(0.0, 1.0, 0.0, 1.0),
+                3 => Color::srgba(0.9, 0.9, 0.1, 1.0),
+                4 => Color::srgba(1.0, 0.8, 0.6, 1.0),
+                _ => Color::srgba(0.2, 0.1, 0.5, 1.0),
+            },
+        );
+        // We generate a text bundle
+        Text2dBundle {
+            text: Text {
+                sections: vec![TextSection {
+                    value: text,
+                    style: TextStyle {
+                        color,
+                        font,
+                        font_size: size,
+                    },
+                }],
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(0., 0., 1.),
+            ..Default::default()
+        }
+    }
+
+    fn spawn_tile(
+        parent: &mut ChildBuilder,
+        tile_map: &TileMap,
+        size: f32,
+        padding: f32,
+        color: Color,
+        bomb_image: Handle<Image>,
+        font: Handle<Font>,
+    ) {
+        for (y, line) in tile_map.iter().enumerate(){
+            for(x, tile) in line.iter().enumerate(){
+                let coordinates = Coordinates {
+                    x: x as u16,
+                    y: y as u16,
+                };
+                let mut cmd = parent.spawn_empty();
+                cmd.insert(SpriteBundle {
+                    sprite: Sprite {
+                        color,
+                        custom_size: Some(Vec2::splat(size - padding)),
+                        ..Default::default()
+                    },
+                    transform: Transform::from_xyz(
+                        (x as f32 * size) + (size / 2.),
+                        (y as f32 * size) + (size / 2.),
+                        1.,
+                    ),
+                    ..Default::default()
+                })
+                .insert(Name::new(format!("Tile ({}, {})",x, y)))
+                .insert(coordinates);
+                match tile {
+                    Tile::Bomb => {
+                        cmd.insert(Bomb);
+                        cmd.with_children(|parent| {
+                            parent.spawn(SpriteBundle {
                                 sprite: Sprite {
-                                    color: Color::hsv(0., 1., 1.),
-                                    custom_size: Some(Vec2::splat(
-                                        tile_size - options.tile_padding as f32,
-                                    )),
+                                    custom_size: Some(Vec2::splat(size - padding)),
                                     ..Default::default()
                                 },
-                                transform: Transform::from_xyz(
-                                    (x as f32 * tile_size) + (tile_size / 2.),
-                                    (y as f32 * tile_size) + (tile_size / 2.),
-                                    1.),
-                                    ..Default::default()
-                            })
-                            .insert(Name::new(format!("Tile ({}, {})",x,y)))
-                            .insert(Coordinates {
-                                x: x as u16,
-                                y: y as u16,
+                                transform: Transform::from_xyz(0., 0., 1.),
+                                texture: bomb_image.clone(),
+                                ..Default::default()
                             });
+                        });
                     }
+                    Tile::BombNeighbor(v) => {
+                        cmd.insert(BombNeighbor {count: *v});
+                        cmd.with_children(|parent| {
+                            parent.spawn(Self::bomb_count_text_bundle(
+                                *v,
+                                font.clone(),
+                                size - padding,
+                            ));
+                        });
+                    }
+                    Tile::Empty => (),
                 }
-                });
+            }
+        }
     }
 
     fn adaptive_tile_size (
